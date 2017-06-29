2fbd1ef75fe52cf0d37f00c4421dd5767f35ba87
Oleg Kozintsev <o.kozintsev@gmail.com>
2017-06-29 17:24:59 +0400

    refactoring

--- index.php
+++ index.php
@@ -1,13 +1,33 @@
 <?
-require_once('lib/curl_query.php');
-require_once('lib/simple_html_dom.php');
+require_once("lib/curl_query.php");
+require_once("lib/simple_html_dom.php");
 
-$url = 'https://www.goodfon.ru/catalog/nature/';
-$data = curl_get($url);
-// Create a DOM object
-$dom = new simple_html_dom();
-// Load HTML from a string
-$dom->load($data);
+$host = "https://www.goodfon.ru";
 
-echo $data;
-#echo $dom;
+$start_url = $host . "/catalog/nature/";
+$html = curl_get($start_url);
+
+$content = str_get_html($html);
+// первая картинка /html/body/div[1]/div[11]/div/a вторая /html/body/div[1]/div[12]/div/a
+$elems = $content->find("/html/body/div[1]/div[11]/div/a");
+//<a href="/wallpaper/priroda-leto-pole-lavanda-solntse.html" title="Природа, лаванда, поле, солнце, лето"
+foreach($elems as $a){
+    $msg = $a->title; // текст пишем в инсаграмм пост
+    $photo_name = str_replace('.html', '', $a->href);
+    $photo_name = str_replace("/wallpaper/", '', $photo_name); // priroda-leto-pole-lavanda-solntse 
+    $html = curl_get($host . $a->href, $start_url);
+    //https://www.goodfon.ru/download/priroda-leto-pole-lavanda-solntse/1080x960/
+    $html = curl_get($host . "/download/" . $photo_name . "/1080x960/", $host . $a->href);
+    $c = str_get_html($html);
+    $es = $c->find("//*[@id=\"im\"]/img");
+    foreach($es as $e){
+        $img_url = $e->src;
+        echo $img_url;     
+        $data = curl_get($img_url);
+        $filename = uniqid() . ".jpg"; // имя файла которе будем отправлять
+        file_put_contents($filename, $data);  
+
+        unlink($filename); // удаляем файл после отправки
+    }
+    break;
+}
